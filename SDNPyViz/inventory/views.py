16c1f0324ea1c30614d4652abfa99f934433995b
subhash <subhash_singh@criterionnetworks.com>
2014-12-20 14:42:08 +0530

    to be worked

--- SDNPyViz/inventory/views.py
+++ SDNPyViz/inventory/views.py
@@ -1,3 +1,13 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.views.generic import View
+import requests
 # Create your views here.
+
+def getNodes(request):
+    """
+    this function retrieve the nodes.
+    :return: json for the available node
+    """
+
+    return HttpResponse('this is working')
